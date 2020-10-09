88be14ac7fc68ee2ecc3f2e3d813b595d301c64f
Kyle Rauzi <krauzi@lclark.edu>
2020-10-08 23:50:08 -0700

    update label colors

--- screens.js
+++ screens.js
@@ -27,26 +27,26 @@ let allScreens = {
     type: 'multiple_choice',
     text: 'You wake up at the NCA Cheer finals in Daytona Florida. You are competing for Navarro College under legend Monica Almada. Gabi Butler is concussed and you are sent in by Monica to compete in her place and must choose a partner.',
     prompts: [
-      { label: 'Morgan', nextScreen: 'morgan_screen' },
-      { label: 'Mackenzie', nextScreen: 'makenzie_screen' },
+      { label: 'Morgan'.red, nextScreen: 'morgan_screen' },
+      { label: 'Mackenzie'.red, nextScreen: 'makenzie_screen' },
     ]
   },
   'morgan_screen': {
     type: 'multiple_choice',
     text: 'You picked Morgan! Decide which move to pull off. Choices are Tripple Canopy Banana Split Reverse (TCBSR), David Decastro Acceleration (DDA), and Stunt Tossup Double Destabilizer (STUDD)',
     prompts: [
-      { label: 'TCBSR', nextScreen: 'morgan_TCBSR' },
-      { label: 'DDA', nextScreen: 'morgan_DDA' },
-      { label: 'STUDD', nextScreen: 'morgan_STUDD' },
+      { label: 'TCBSR'.red, nextScreen: 'morgan_TCBSR' },
+      { label: 'DDA'.red, nextScreen: 'morgan_DDA' },
+      { label: 'STUDD'.red, nextScreen: 'morgan_STUDD' },
     ]
   },
   'makenzie_screen': {
     type: 'multiple_choice',
     text: 'You picked Makenzie! Decide which move to pull off. Choices are Tripple Canopy Banana Split Reverse, David Decastro Acceleration, and Stunt Tossup Double Destabilizer',
     prompts: [
-      { label: 'TCBSR', nextScreen: 'makenzie_TCBSR' },
-      { label: 'DDA', nextScreen: 'makenzie_DDA' },
-      { label: 'STUDD', nextScreen: 'makenzie_STUDD' },
+      { label: 'TCBSR'.red, nextScreen: 'makenzie_TCBSR' },
+      { label: 'DDA'.red, nextScreen: 'makenzie_DDA' },
+      { label: 'STUDD'.red, nextScreen: 'makenzie_STUDD' },
     ]
   },
   'morgan_TCBSR': {
@@ -80,33 +80,33 @@ let allScreens = {
     type: 'multiple_choice',
     text: 'Jesse is called to take the place of the last batter in the world series for the Oakland As after Matt Olson has hyper extended his vastus laterals. You must pick a bat blessed by several Oakland As players and use it to defeat the Detroit Tigers.',
     prompts: [
-      { label: 'Jesus Lezardo bat', nextScreen: 'JL_screen' },
-      { label: 'Tommy La Stella bat', nextScreen: 'TLS_screen' },
-      { label: 'Matt Chapman bat', nextScreen: 'MC_screen' },
+      { label: 'Jesus Lezardo bat'.red, nextScreen: 'JL_screen' },
+      { label: 'Tommy La Stella bat'.red, nextScreen: 'TLS_screen' },
+      { label: 'Matt Chapman bat'.red, nextScreen: 'MC_screen' },
     ]
   },
   'JL_screen': {
     type: 'multiple_choice',
     text: 'You picked JL bat! Nice. Choose your MLB power snack',
     prompts: [
-      { label: 'Big League Chew', nextScreen: 'JL_BLC' },
-      { label: 'Sunflower Seeds', nextScreen: 'JL_SS' },
+      { label: 'Big League Chew'.red, nextScreen: 'JL_BLC' },
+      { label: 'Sunflower Seeds'.red, nextScreen: 'JL_SS' },
     ]
   },
   'TLS_screen': {
     type: 'multiple_choice',
     text: 'You picked TLS bat! Nice. Choose your MLB power snack',
     prompts: [
-      { label: 'Big League Chew', nextScreen: 'TLS_BLC' },
-      { label: 'Sunflower Seeds', nextScreen: 'TLS_SS' },
+      { label: 'Big League Chew'.red, nextScreen: 'TLS_BLC' },
+      { label: 'Sunflower Seeds'.red, nextScreen: 'TLS_SS' },
     ]
   },
   'MC_screen': {
     type: 'multiple_choice',
     text: 'You picked MC bat! Nice. Choose your MLB power snack',
     prompts: [
-      { label: 'Big League Chew', nextScreen: 'MC_BLC' },
-      { label: 'Sunflower Seeds', nextScreen: 'MC_SS' },
+      { label: 'Big League Chew'.red, nextScreen: 'MC_BLC' },
+      { label: 'Sunflower Seeds'.red, nextScreen: 'MC_SS' },
     ]
   },
   'JL_BLC': {
@@ -131,7 +131,7 @@ let allScreens = {
   },
   'MC_SS': {
     isEndingScreen: true,
-    text: 'The ghost of legendary but controversial Tigers player Ty Cobb retursn from the dead and throws a pitch. You hit the ball to another surprisingly undead player Hank Greenburg. You leave the game disturbed by the rise of dead players coming back to life.',
+    text: 'The ghost of legendary but controversial Tigers player Ty Cobb returns from the dead and throws a pitch. You hit the ball to another surprisingly undead player Hank Greenburg. You leave the game disturbed by the rise of dead players coming back to life.',
   },
 
 
@@ -141,8 +141,8 @@ let allScreens = {
     type: 'multiple_choice',
     text: 'You picked Terra! Nice. You and your family have recently moved from Colorado to Racoon city. However, The Umbrella Corporation is still up to their dirty ways and releases a new strain of zombie virus on the population, causing another zombie outbreak. Now, in the midst of all the choas and confusion, a zombie has broken into your house. You see a gun and a fresh banana. What weapon will you choose?',
     prompts: [
-      { label: 'Banana', nextScreen:'banana_screen'  },
-      { label: 'Gun', nextScreen: 'gun_screen' }, // mistake was nextscreen vs nextScreen. It was the lower case s
+      { label: 'Banana'.red, nextScreen:'banana_screen'  },
+      { label: 'Gun'.red, nextScreen: 'gun_screen' }, // mistake was nextscreen vs nextScreen. It was the lower case s
     ]
   },
   'banana_screen': {
@@ -153,16 +153,16 @@ let allScreens = {
     type: 'multiple_choice',
     text: 'You pick up the gun and kill the zombie, avoiding being eaten alive by the foul creature of the undead. You are now faced with a choice to search for your family in the house or make a run for it. Which will you choose?',
       prompts: [
-        { label: 'Search for family', nextScreen: 'search_screen' },
-        { label: 'Leave family and exit the house', nextScreen: 'leave_screen' },
+        { label: 'Search for family'.red, nextScreen: 'search_screen' },
+        { label: 'Leave family and exit the house'.red, nextScreen: 'leave_screen' },
       ]
   },
   'search_screen': {
     type: 'multiple_choice',
     text: 'You find your family and collect them safely and exit the house, but you are faced with another choice. Walk around on the streets or get in the car and drive?',
     prompts: [
-      { label: 'Get in the car and drive', nextScreen: 'familycar_screen' },
-      { label: 'Walk down the street', nextScreen: 'familyambush_screen' },
+      { label: 'Get in the car and drive'.red, nextScreen: 'familycar_screen' },
+      { label: 'Walk down the street'.red, nextScreen: 'familyambush_screen' },
     ]
   },
   'leave_screen': {
@@ -173,8 +173,8 @@ let allScreens = {
       type: 'multiple_choice',
       text: 'You take your family and drive in the car, making your way to a crossroads. Do you drive into the city or outside of the city limits?',
       prompts: [
-          { label: 'Drive into the city to try and get supplies and search for other survivors', nextScreen:'familydeath_screen' },
-          { label: 'Get out of the city, with no food or water, and limited gas in your car', nextScreen: 'familylive_screen' }
+          { label: 'Drive into the city to try and get supplies and search for other survivors'.red, nextScreen:'familydeath_screen' },
+          { label: 'Get out of the city, with no food or water, and limited gas in your car'.red, nextScreen: 'familylive_screen' }
       ]
   },
   'familyambush_screen': {
@@ -214,7 +214,7 @@ function displayScreen(screenName) {
       console.log(`[${position}] ${prompt.label}`)
     }
 
-    let choice = readlineSync.question('What is your choice? ');
+    let choice = readlineSync.question('What is your choice?'.green);
 
     // TODO: Handle bad user input (not a number, out of range, etc.)
     let index = parseInt(choice) - 1;
