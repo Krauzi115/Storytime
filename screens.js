3292436a21f77be67c24d1aad6e3b27565c56770
Kevin Chen <kevinch0514@gmail.com>
2020-10-08 23:50:34 -0600

    fixed and edited Terra

--- screens.js
+++ screens.js
@@ -114,11 +114,6 @@ let allScreens = {
       { label: 'Sunflower Seeds', nextScreen: 'MC_SS' },
     ]
   },
- 
- 
- 
- 
- 
   'JL_BLC': {
     isEndingScreen: true,
     text: 'Lose!',
@@ -152,103 +147,69 @@ let allScreens = {
 
 
 
-
-
-
-
-
-
+  // TERRA
   'terra_intro': {
     type: 'multiple_choice',
-    text: 'You picked Terra! Nice. Terra and her family have recently moved from Colorado to Racoon city. However, The Umbrella Corporation is still up to their dirty ways and releases a new starin of zombie virus on the population, causing another zombie outbreak. Now in the midst of all the choas and confucion, a zombie has broken into her house. Terra sees a gun and a fresh banana. What weapon will she choose?' ,
+    text: 'You picked Terra! Nice. You and your family have recently moved from Colorado to Racoon city. However, The Umbrella Corporation is still up to their dirty ways and releases a new strain of zombie virus on the population, causing another zombie outbreak. Now, in the midst of all the choas and confusion, a zombie has broken into your house. You see a gun and a fresh banana. What weapon will you choose?',
     prompts: [
       { label: 'Banana', nextScreen:'banana_screen'  },
-      { label: 'Gun', nextscreen: 'gun_screen' },
+      { label: 'Gun', nextScreen: 'gun_screen' }, // mistake was nextscreen vs nextScreen. It was the lower case s
     ]
   },
-
   'banana_screen': {
-      isEndingScreen: true,
-      text: 'Terra enjoyed her final meal of a fresh banana, but it was not an effective weapon against the zombie. The zombie ate her, game over'
+    isEndingScreen: true,
+    text: 'You enjoyed your final meal of a fresh banana, but it was not an effective weapon against the zombie. The zombie ate you, game over',
   },
-
-
   'gun_screen': {
-      type: 'multiple_choice',
-      text: 'Terra picks up the gun and kills the zombie, avoiding being eaten alive by the foul creature of the undead. You are now faced with a choice to search for your family in the house or make a run for it. Which will you choose?',
-        prompts: [
-            { label: 'Search for family', nextScreen: 'search_screen' },
-            { label: 'leave family and exit the house', nextScreen: 'leave_screen' }
-        ]
-    },
-
-
-    'search_screen': {
-        type: 'multiple_choice',
-        text: 'You find your family and collect them safely and exit the house, but you are faced with another choice. Walk around on the streets or get in the car and drive?',
-        prompts: [
-          { label: 'Get in the car and drive', nextScreen: 'familycar_screen' },
-          { label: 'Walk down the street', nextScreen: 'familyambush_screen' },
-        ]
-      },
-
-      'familycar_screen': {
-          type: 'multiple_choice',
-          text: 'You take your family and drive in the car, making your way to a crossroads. Do you drive into the city or outside of the city limits?',
-          prompts: [
-              { label: 'Drive into the city to try and get supplies and search for other survivors', nextScreen:'familydeath_screen' },
-              { label: 'Get out of the city, with no food or water, and limited gas in your car', nextScreen: 'familylive_screen' }
-          ]
-      },
-
-        'familylive_screen': {
-            isEndingScreen: true,
-            text: 'Terra and her family have successfully driven outside of Racoon City escaping the evil horde of the undead and the Umbrealla Corporation',
-        },
-
-        'familydeath_screen': {
-            isEndingScreen: true, 
-            text: 'You drive into the city and are rushed by the entire infected city population, worst of all the Umbrella Corporation has sent in soldiers to clean the scene. You and your family have perished.'
-        },
-
-        'familyambush_screen': {
-           isEndingScreen: true,
-           text: 'You walked into the street with your family and were all attacked by your undead neighbors. You have all perished.'   
-        },
-
-        'leave_screen': {
-            isEndingScreen: true, 
-            text: 'You have escaped the house and but feel guilty about leaving your family, so you rush back inside to save them but it is too late, they have become zombies and eat you'
-            
-        },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  'game_over': {
-    isEndingScreen: true,
-    text: 'All done!',
-    prompts: []
+    type: 'multiple_choice',
+    text: 'You pick up the gun and kill the zombie, avoiding being eaten alive by the foul creature of the undead. You are now faced with a choice to search for your family in the house or make a run for it. Which will you choose?',
+      prompts: [
+        { label: 'Search for family', nextScreen: 'search_screen' },
+        { label: 'Leave family and exit the house', nextScreen: 'leave_screen' },
+      ]
   },
-  'game_over': {
+  'search_screen': {
+    type: 'multiple_choice',
+    text: 'You find your family and collect them safely and exit the house, but you are faced with another choice. Walk around on the streets or get in the car and drive?',
+    prompts: [
+      { label: 'Get in the car and drive', nextScreen: 'familycar_screen' },
+      { label: 'Walk down the street', nextScreen: 'familyambush_screen' },
+    ]
+  },
+  'leave_screen': {
     isEndingScreen: true,
-    text: 'All done!',
-    prompts: []
-  }
+    text: 'You have escaped the house and but feel guilty about leaving your family, so you rush back inside to save them but it is too late, they have become zombies and eat you.'
+  },
+  'familycar_screen': {
+      type: 'multiple_choice',
+      text: 'You take your family and drive in the car, making your way to a crossroads. Do you drive into the city or outside of the city limits?',
+      prompts: [
+          { label: 'Drive into the city to try and get supplies and search for other survivors', nextScreen:'familydeath_screen' },
+          { label: 'Get out of the city, with no food or water, and limited gas in your car', nextScreen: 'familylive_screen' }
+      ]
+  },
+  'familyambush_screen': {
+    isEndingScreen: true,
+    text: 'You walked into the street with your family and were all attacked by your undead neighbors. You have all perished.'
+  },
+  'familydeath_screen': {
+    isEndingScreen: true,
+    text: 'You drive into the city and are rushed by the entire infected city population. Worst of all, the Umbrella Corporation has sent in soldiers to clean the scene. You and your family have perished.'
+  },
+  'familylive_screen': {
+    isEndingScreen: true,
+    text: 'You and your family have successfully driven outside of Racoon City escaping the evil horde of the undead and the Umbrella Corporation',
+  },
+  // 'game_over': {
+  //   isEndingScreen: true,
+  //   text: 'All done!',
+  //   prompts: []
+  // },
+  // 'game_over': {
+  //   isEndingScreen: true,
+  //   text: 'All done!',
+  //   prompts: []
+  // }
 };
 
 function displayScreen(screenName) {
